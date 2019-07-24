54a5eb8816cdcfc80ac0d7d37e95b3e9f02edc53
Devin <aebid@tamu.edu>
2019-07-24 16:12:24 -0500

    Added plotting profiles. Uses average value per bin.

--- make_plot.py
+++ make_plot.py
@@ -1,5 +1,6 @@
 import os
 import ROOT
+import array
 ROOT.gROOT.SetBatch(1)
 ROOT.gStyle.SetOptFit(1011)
 
@@ -32,6 +33,22 @@ def plot1Dfit(name, title, event, size, xlabel, ylabel, cut, plotdir):
 	c1.SaveAs("plots/"+plotdir+"/"+title+".png")
 	return hist
 
+
+def plot1Dprof(name, title, event, size, xlabel, ylabel, cut, plotdir):
+        if os.path.exists("plots/"+plotdir) == False:
+                os.mkdir("plots/"+plotdir)
+        c1 = ROOT.TCanvas("", "", 800, 600)
+        hist = ROOT.TProfile(name, title, size[0], size[1], size[2], size[4], size[5])
+        hist.GetXaxis().SetTitle(xlabel)
+        hist.GetYaxis().SetTitle(ylabel)
+	hist.GetYaxis().SetRangeUser(size[4], size[5])
+        hist.Sumw2()
+        event.Project(name, name, cut)
+        hist.Draw("Hist")
+        c1.SaveAs("plots/"+plotdir+"/"+title+".png")
+        return hist
+
+
 def plot2Dhist(name, title, event, size, xlabel, ylabel, cut, plotdir):
 	if os.path.exists("plots/"+plotdir) == False:
                 os.mkdir("plots/"+plotdir)
@@ -54,6 +71,17 @@ def plot2Ddensity(name, title, event, size, xlabel, ylabel, cut, plotdir):
 	prof.GetYaxis().SetTitle(ylabel)
 	prof.Sumw2()
 	event.Project(name, name, cut)
+	
+	ablue = array.array("d", [1,1,0])
+	ared = array.array("d", [0,1,1])
+	agreen = array.array("d", [0,1,0])
+	astop = array.array("d", [0,.5,1])
+	myPalette = []
+	fi = ROOT.TColor.CreateGradientColorTable(3, astop, ared, agreen, ablue, 100)
+	for x in range(100):
+	  myPalette.append(fi+x)
+	ROOT.gStyle.SetPalette(100, array.array("i", myPalette))
+	prof.GetZaxis().SetRangeUser(size[6], size[7])
 	prof.Draw("colz")
 	c1.SaveAs("plots/"+plotdir+"/"+title+".png")
 	return prof
--- plots.py
+++ plots.py
@@ -1,67 +1,118 @@
 import ROOT, os
-from make_plot import plot1Dhist, plot1Dfit, plot2Dhist, plot2Ddensity
+from make_plot import plot1Dhist, plot1Dfit, plot1Dprof, plot2Dhist, plot2Ddensity
 
-f = ROOT.TFile("GEM_100pt.root")
+f = ROOT.TFile("GEM_30pt.root")
 
 event = f.Get("SliceTestAnalysis/MuonData")
 
-if os.path.exists("plots/") == False:
+if os.path.exists("plots") == False:
   os.mkdir("plots/")
+
 
 
 ###################################################1D HIST###################################################
 #############################################################################################################
-
-plot1Dhist("rechit_prop_dX_GE11", "local_x_dx", event, [100, -4, 4], "x(cm)", "", "", "residuals")
-plot1Dhist("rechit_localx_GE11 - prop_localx_GE11", "local_x_sub", event, [100, -4, 4], "x(cm)", "", \
-           "", "residuals")
-plot1Dhist("rechit_phi_GE11 -  prop_phi_GE11", "global_phi", event, [100, -.04, .04], "phi", "", \
-           "", "residuals")
-plot1Dhist("rechit_localx_GE11 - prop_localx_center_GE11", "local_x_centered", event, [100, -2, 2], "", \
-           "", "", "residuals")
-
-
-plot1Dhist("rechit_prop_dX_GE11_foralignment", "standalone_prop_res", event, [100, -4, 4], "x(cm)", "hits", \
-           "", "residuals")
-plot1Dhist("rechit_propgt_dX_GE11_foralignment", "gt_prop_res", event, [100, -4, 4], "x(cm)", "hits", \
-           "", "residuals")
-plot1Dhist("rechit_propinner_dX_GE11_foralignment", "inner_prop_res", event, [100, -4, 4], "x(cm)", "hits", \
-           "", "residuals")
+plot1Dhist("rechit_prop_RdPhi_GE11", "standalone_prop_res", event, [100, -4, 4], \
+           "Rphi (cm)", "hits", "", "residuals")
+plot1Dhist("rechit_propgt_RdPhi_GE11", "gt_prop_res", event, [100, -4, 4], \
+           "Rphi (cm)", "hits", "", "residuals")
+plot1Dhist("rechit_propinner_RdPhi_GE11", "inner_prop_res", event, [100, -4, 4], \
+           "Rphi (cm)", "hits", "", "residuals")
 #############################################################################################################
 
 
 
 ###################################################2D HIST###################################################
 #############################################################################################################
+plot2Dhist("rechit_x_GE11:rechit_y_GE11", "rec_rec", event, [1000, -300, 300, 1000, -300, 300], \
+           "", "", "", "global_density")
+plot2Dhist("prop_x_GE11:prop_y_GE11", "prop_prop", event, [1000, -300, 300, 1000, -300, 300], \
+           "", "", "", "global_density")
+#############################################################################################################
 
-plot2Dhist("rechit_x_GE11:rechit_y_GE11", "rec_rec", event, [1000, -300, 300, 1000, -300, 300], "", "", \
-           "", "global_desnity")
-plot2Dhist("prop_x_GE11:prop_y_GE11", "prop_prop", event, [1000, -300, 300, 1000, -300, 300], "", "", \
-           "", "global_density")
-plot2Dhist("prop_x_GE11:rechit_x_GE11", "prop_rec", event, [100, -30, 30, 100, -30, 30], "", "", \
-           "", "global_density")
-plot2Dhist("prop_phi_GE11:rechit_phi_GE11", "prop_rec_phi", event, [100, -4, 4, 100, -4, 4], "", \
-           "", "", "global_density")
-plot2Dhist("prop_localx_GE11:rechit_localx_GE11", "prop_rec", event, [100, -30, 30, 100, -30, 30], \
-           "", "", "", "local_density")
 
+
+###################################################1D PROF###################################################
+#############################################################################################################
+plot1Dprof("rechit_prop_RdPhi_GE11:rechit_stripangle_GE11", "residual_by_angle_odd", event, \
+           [400, -.1, .1, 200, -.1, .1], "", "", "chamber_GE11%2 == 1", "stand_even_odd_res")
+plot1Dprof("rechit_prop_RdPhi_GE11:prop_strip_GE11", "residual_by_number_odd", event, \
+           [400, -10, 390, 200, -.1, .1], "", "", "chamber_GE11%2 == 1", "stand_even_odd_res")
+plot1Dprof("rechit_prop_RdPhi_GE11:rechit_stripangle_GE11", "residual_by_angle_even", event, \
+           [400, -.1, .1, 200, -.1, .1], "", "", "chamber_GE11%2 == 0", "stand_even_odd_res")
+plot1Dprof("rechit_prop_RdPhi_GE11:prop_strip_GE11", "residual_by_number_even", event, \
+           [400, -10, 390, 200, -.1, .1], "", "", "chamber_GE11%2 == 0", "stand_even_odd_res")
+
+plot1Dprof("rechit_prop_RdPhi_GE11:rechit_stripangle_GE11", "residual_by_angle_odd_high", event, \
+           [400, -.1, .1, 200, -15, 15], "", "", "chamber_GE11%2 == 1", "stand_even_odd_res")
+plot1Dprof("rechit_prop_RdPhi_GE11:prop_strip_GE11", "residual_by_number_odd_high", event, \
+           [400, -10, 390, 200, -15, 15], "", "", "chamber_GE11%2 == 1", "stand_even_odd_res")
+plot1Dprof("rechit_prop_RdPhi_GE11:rechit_stripangle_GE11", "residual_by_angle_even_high", event, \
+           [400, -.1, .1, 200, -15, 15], "", "", "chamber_GE11%2 == 0", "stand_even_odd_res")
+plot1Dprof("rechit_prop_RdPhi_GE11:prop_strip_GE11", "residual_by_number_even_high", event, \
+           [400, -10, 390, 200, -15, 15], "", "", "chamber_GE11%2 == 0", "stand_even_odd_res")
+
+plot1Dprof("rechit_propgt_RdPhi_GE11:rechit_stripangle_GE11", "residual_by_angle_odd", event, \
+           [400, -.1, .1, 200, -.1, .1], "", "", "chamber_GE11%2 == 1", "gt_even_odd_res")
+plot1Dprof("rechit_propgt_RdPhi_GE11:prop_strip_GE11", "residual_by_number_odd", event, \
+           [400, -10, 390, 200, -.1, .1], "", "", "chamber_GE11%2 == 1", "gt_even_odd_res")
+plot1Dprof("rechit_propgt_RdPhi_GE11:rechit_stripangle_GE11", "residual_by_angle_even", event, \
+           [400, -.1, .1, 200, -.1, .1], "", "", "chamber_GE11%2 == 0", "gt_even_odd_res")
+plot1Dprof("rechit_propgt_RdPhi_GE11:prop_strip_GE11", "residual_by_number_even", event, \
+           [400, -10, 390, 200, -.1, .1], "", "", "chamber_GE11%2 == 0", "gt_even_odd_res")
+
+plot1Dprof("rechit_propgt_RdPhi_GE11:rechit_stripangle_GE11", "residual_by_angle_odd_high", event, \
+           [400, -.1, .1, 200, -15, 15], "", "", "chamber_GE11%2 == 1", "gt_even_odd_res")
+plot1Dprof("rechit_propgt_RdPhi_GE11:prop_strip_GE11", "residual_by_number_odd_high", event, \
+           [400, -10, 390, 200, -15, 15], "", "", "chamber_GE11%2 == 1", "gt_even_odd_res")
+plot1Dprof("rechit_propgt_RdPhi_GE11:rechit_stripangle_GE11", "residual_by_angle_even_high", event, \
+           [400, -.1, .1, 200, -15, 15], "", "", "chamber_GE11%2 == 0", "gt_even_odd_res")
+plot1Dprof("rechit_propgt_RdPhi_GE11:prop_strip_GE11", "residual_by_number_even_high", event, \
+           [400, -10, 390, 200, -15, 15], "", "", "chamber_GE11%2 == 0", "gt_even_odd_res")
+
+plot1Dprof("rechit_propinner_RdPhi_GE11:rechit_stripangle_GE11", "residual_by_angle_odd", event, \
+           [400, -.1, .1, 200, -.1, .1], "", "", "chamber_GE11%2 == 1", "inner_even_odd_res")
+plot1Dprof("rechit_propinner_RdPhi_GE11:prop_strip_GE11", "residual_by_number_odd", event, \
+           [400, -10, 390, 200, -.1, .1], "", "", "chamber_GE11%2 == 1", "inner_even_odd_res")
+plot1Dprof("rechit_propinner_RdPhi_GE11:rechit_stripangle_GE11", "residual_by_angle_even", event, \
+           [400, -.1, .1, 200, -.1, .1], "", "", "chamber_GE11%2 == 0", "inner_even_odd_res")
+plot1Dprof("rechit_propinner_RdPhi_GE11:prop_strip_GE11", "residual_by_number_even", event, \
+           [400, -10, 390, 200, -.1, .1], "", "", "chamber_GE11%2 == 0", "inner_even_odd_res")
+
+plot1Dprof("rechit_propinner_RdPhi_GE11:rechit_stripangle_GE11", "residual_by_angle_odd_high", event, \
+           [400, -.1, .1, 200, -15, 15], "", "", "chamber_GE11%2 == 1", "inner_even_odd_res")
+plot1Dprof("rechit_propinner_RdPhi_GE11:prop_strip_GE11", "residual_by_number_odd_high", event, \
+           [400, -10, 390, 200, -15, 15], "", "", "chamber_GE11%2 == 1", "inner_even_odd_res")
+plot1Dprof("rechit_propinner_RdPhi_GE11:rechit_stripangle_GE11", "residual_by_angle_even_high", event, \
+           [400, -.1, .1, 200, -15, 15], "", "", "chamber_GE11%2 == 0", "inner_even_odd_res")
+plot1Dprof("rechit_propinner_RdPhi_GE11:prop_strip_GE11", "residual_by_number_even_high", event, \
+           [400, -10, 390, 200, -15, 15], "", "", "chamber_GE11%2 == 0", "inner_even_odd_res")
 #############################################################################################################
 
 
 
 ###################################################DENSITY###################################################
 #############################################################################################################
+cut = "abs(middle_perp_propGE11) < 1000"
+plot2Ddensity("rechit_prop_RdPhi_GE11:prop_localy_GE11+middle_perp_propGE11:prop_localx_GE11", \
+              "standalone_prop_residual", event, [50, -25, 25, 100, 120, 250, -.5, .5], \
+              "", "", cut+"&& abs(rechit_prop_RdPhi_GE11) < 10", "res_density_GE11")
+plot2Ddensity("rechit_propgt_RdPhi_GE11:prop_localy_GE11+middle_perp_propGE11:prop_localx_GE11", \
+              "gt_prop_residual", event, [50, -25, 25, 100, 120, 250, -.5, .5], \
+              "", "", cut+"&& abs(rechit_propgt_RdPhi_GE11) < 10", "res_density_GE11")
+plot2Ddensity("rechit_propinner_RdPhi_GE11:prop_localy_GE11+middle_perp_propGE11:prop_localx_GE11", \
+              "inner_prop_residual", event, [50, -25, 25, 100, 120, 250, -.5, .5], \
+              "", "", cut+"&& abs(rechit_propinner_RdPhi_GE11) < 10", "res_density_GE11")
 
-plot2Ddensity("rechit_prop_dX_GE11_foralignment:prop_localy_GE11:prop_localx_GE11", "standalone_prop_residual", \
-              event, [50, -30, 30, 50, -15, 15], "", "", "abs(rechit_prop_dX_GE11_foralignment) < 10", \
-              "res_density")
-plot2Ddensity("rechit_propgt_dX_GE11_foralignment:prop_localy_GE11:prop_localx_GE11", "gt_prop_residual", \
-              event, [50, -30, 30, 50, -15, 15], "", "", "abs(rechit_propgt_dX_GE11_foralignment) < 10", \
-              "res_density")
-plot2Ddensity("rechit_propinner_dX_GE11_foralignment:prop_localy_GE11:prop_localx_GE11", \
-              "inner_prop_residual", event, [50, -30, 30, 50, -15, 15], "", "", \
-              "abs(rechit_propinner_dX_GE11_foralignment) < 10", "res_density")
-
+plot2Ddensity("rechit_prop_RdPhi_ME11:prop_localy_ME11:prop_localx_ME11", \
+              "standalone_prop_residual_ME11", event, [100, -40, 40, 100, -100, 100, -2, 2], \
+              "", "", "abs(rechit_prop_RdPhi_ME11) < 10 && abs(prop_localy_ME11) < 70", "res_density_ME11")
+plot2Ddensity("rechit_propgt_RdPhi_ME11:prop_localy_ME11:prop_localx_ME11", \
+              "gt_prop_residual_ME11", event, [100, -40, 40, 100, -100, 100, -2, 2], \
+              "", "", "abs(rechit_propgt_RdPhi_ME11) < 10", "res_density_ME11")
+plot2Ddensity("rechit_propinner_RdPhi_ME11:prop_localy_ME11:prop_localx_ME11", \
+              "inner_prop_residual_ME11", event, [100, -40, 40, 100, -100, 100, -2, 2], \
+              "", "", "abs(rechit_propinner_RdPhi_ME11) < 10", "res_density_ME11")
 ############################################################################################################
 
 
@@ -100,11 +151,8 @@ for i in range(2):
 for i in range(2):
   for j in range(1,9):
     cut = "roll_GE11[{}] == {}".format(i,k)
-    plot1Dhist("rechit_prop_dX_GE11[{}]".format(i), "Local_res_layer{}_roll{}".format(i,k), event, \
-               [100, -4, 4], "x(cm)", "", cut, "dx_localx_LR")
-    plot1Dhist("rechit_localx_GE11[{}] - prop_localx_center_GE11[{}]".format(i,i), \
-               "Local_res_center_layer{}_roll{}".format(i,j), event, [100, -4, 4], "x(cm)", "", \
-               cut, "dx_localx_LR_center")
+    plot1Dhist("rechit_prop_RdPhi_GE11[{}]".format(i), "rdphi_layer{}_roll{}".format(i,k), event, \
+               [100, -3, 3], "Rphi(cm)", "", cut, "rdphi_LR")
 #############################################################################################################
 
 
@@ -112,13 +160,9 @@ for i in range(2):
 ####################################################LAYER####################################################
 #############################################################################################################
 for i in range(2):
-  plot1Dhist("rechit_localx_GE11[{}]".format(i), "Rec_localx_L{}_even".format(i), event, [100, -30, 30], \
-             "x(cm)", "", "chamber_GE11[{}]%2 == 0".format(i), "localx_even_odd")
-  plot1Dhist("rechit_localx_GE11[{}]".format(i), "Rec_localx_L{}_odd".format(i), event, [100, -30, 30], \
-             "x(cm)", "", "chamber_GE11[{}]%2 == 1".format(i), "localx_even_odd")
-  plot1Dhist("prop_localx_GE11[{}]".format(i), "Prop_localx_L{}_even".format(i), event, [100, -30, 30], \
-             "x(cm)", "", "chamber_GE11[{}]%2 == 0".format(i), "localx_even_odd")
-  plot1Dhist("prop_localx_GE11[{}]".format(i), "Prop_localx_L{}_odd".format(i), event, [100, -30, 30], \
-             "x(cm)", "", "chamber_GE11[{}]%2 == 1".format(i), "localx_even_odd")
+  plot1Dhist("rechit_prop_RdPhi_GE11[{}]".format(i), "rphi_res_L{}_even".format(i), event, [100, -3, 3], \
+             "Rphi (cm)", "", "chamber_GE11[{}]%2 == 0".format(i), "rdphi_even_odd")
+  plot1Dhist("rechit_prop_RdPhi_GE11[{}]".format(i), "rphi_res_L{}_odd".format(i), event, [100, -3, 3], \
+             "Rphi (cm)", "", "chamber_GE11[{}]%2 == 1".format(i), "rdphi_even_odd")
 #############################################################################################################
-"""
+
